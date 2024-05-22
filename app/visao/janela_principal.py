12cb7dca3172afd1ee1cbc8e17e81e2b384e7e06
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-05-22 19:40:49 -0300

    Renomeando referência do pacote importado

--- app/visao/janela_principal.py
+++ app/visao/janela_principal.py
@@ -1,10 +1,10 @@
-import tkinter as tk
+import tkinter
 
 from controlador.controlador import Controlador
 from visao.home import Home
 
 
-class JanelaPrincipal(tk.Tk):
+class JanelaPrincipal(tkinter.Tk):
     """
     Janela principal
     """
@@ -14,7 +14,7 @@ class JanelaPrincipal(tk.Tk):
         Constrói uma nova instância da JanelaPrinpal.
         :param controlador: Controlador que está criando esta JanelaPrincipal.
         """
-        tk.Tk.__init__(self, *args, **kwargs)
+        tkinter.Tk.__init__(self, *args, **kwargs)
         # Controlador que instanciou a janela principal
         self.controlador = Controlador()
 
@@ -27,10 +27,10 @@ class JanelaPrincipal(tk.Tk):
         self.geometry(f"{screen_width}x{screen_height}+0+0")
 
         # Barra de menu
-        barra_menu = tk.Menu(self)
+        barra_menu = tkinter.Menu(self)
 
         # Menu arquivo
-        menu_arquivo = tk.Menu(barra_menu, tearoff=0)
+        menu_arquivo = tkinter.Menu(barra_menu, tearoff=0)
         menu_arquivo.add_command(
             label="Abrir imagem",
             command=lambda: self.controlador.abrir_arquivo_imagem(self.adicionar_imagem)
@@ -39,11 +39,11 @@ class JanelaPrincipal(tk.Tk):
         barra_menu.add_cascade(label="Arquivo", menu=menu_arquivo)
 
         # Menu editar
-        menu_editar = tk.Menu(barra_menu, tearoff=0)
+        menu_editar = tkinter.Menu(barra_menu, tearoff=0)
         barra_menu.add_cascade(label="Editar", menu=menu_editar)
 
         # Menu visualizar
-        menu_visualizar = tk.Menu(barra_menu, tearoff=0)
+        menu_visualizar = tkinter.Menu(barra_menu, tearoff=0)
         menu_visualizar.add_command(label="Exibir cores")
         menu_visualizar.add_command(label="Exibir tons de cinza")
         menu_visualizar.add_command(label="Aumentar Zoom")
@@ -51,7 +51,7 @@ class JanelaPrincipal(tk.Tk):
         barra_menu.add_cascade(label="Visualizar", menu=menu_visualizar)
 
         # Menu converter
-        menu_converter = tk.Menu(barra_menu, tearoff=0)
+        menu_converter = tkinter.Menu(barra_menu, tearoff=0)
         menu_converter.add_command(
             label="Tons de cinza",
             command=lambda: self.controlador.converter_imagem_rgb_para_imagem_tons_cinza()
@@ -59,21 +59,21 @@ class JanelaPrincipal(tk.Tk):
         barra_menu.add_cascade(label="Conveter", menu=menu_converter)
 
         # Menu gerar
-        menu_gerar = tk.Menu(barra_menu, tearoff=0)
-        submenu_histograma = tk.Menu(menu_gerar, tearoff=0)
+        menu_gerar = tkinter.Menu(barra_menu, tearoff=0)
+        submenu_histograma = tkinter.Menu(menu_gerar, tearoff=0)
         submenu_histograma.add_command(label="Tons de cinza")
         submenu_histograma.add_command(label="Cor")
         menu_gerar.add_cascade(label="Histograma", menu=submenu_histograma)
         barra_menu.add_cascade(label="Gerar", menu=menu_gerar)
 
         # Menu caracterizar imagem
-        menu_caracterizar_imagem = tk.Menu(barra_menu, tearoff=0)
+        menu_caracterizar_imagem = tkinter.Menu(barra_menu, tearoff=0)
         menu_caracterizar_imagem.add_command(label="Descritores de Haralick")
         menu_caracterizar_imagem.add_command(label="Momentos invariantes de Hu")
         barra_menu.add_cascade(label="Caracterizar", menu=menu_caracterizar_imagem)
 
         # Menu classificar
-        menu_classificar = tk.Menu(barra_menu, tearoff=0)
+        menu_classificar = tkinter.Menu(barra_menu, tearoff=0)
         menu_classificar.add_command(label="SVM")
         menu_classificar.add_command(label="ResNet50")
         barra_menu.add_cascade(label="Classificar", menu=menu_classificar)
@@ -81,7 +81,7 @@ class JanelaPrincipal(tk.Tk):
         self.config(menu=barra_menu)
 
         # Configurando mudança de página
-        container = tk.Frame(self)
+        container = tkinter.Frame(self)
         container.pack(side="top", fill="x", expand=True)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
@@ -109,6 +109,6 @@ class JanelaPrincipal(tk.Tk):
         :param foto: Imagem a ser inserida.
         :type caminho: ImageTk.
         """
-        legenda = tk.Label(self, image=foto)
+        legenda = tkinter.Label(self, image=foto)
         legenda.image = foto
         legenda.pack()
