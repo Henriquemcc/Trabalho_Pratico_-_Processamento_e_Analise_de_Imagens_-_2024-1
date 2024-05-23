7c9ea1c1ac9b182ea8188c5c35c3f56e9a7a8f11
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-05-23 18:32:34 -0300

    Implementando a conversão de uma instância de ImagemTonsCinza em uma imagem PIL.Image.Image

--- app/modelo/imagem_tons_cinza.py
+++ app/modelo/imagem_tons_cinza.py
@@ -1,3 +1,6 @@
+import PIL.Image
+
+
 class ImagemTonsCinza:
     """
     Representa uma matriz de tons de cinza.
@@ -9,3 +12,10 @@ class ImagemTonsCinza:
         :param matriz: Matriz de tons de cinza.
         """
         self.matriz = matriz
+
+    def to_image(self) -> PIL.Image.Image:
+        """
+        Converte ImagemTonsCinza em uma PIL.Image.Image
+        :return: PIL.Image.Image gerada a partir de ImagemTonsCinza.
+        """
+        return PIL.Image.fromarray(self.matriz, mode='L')
