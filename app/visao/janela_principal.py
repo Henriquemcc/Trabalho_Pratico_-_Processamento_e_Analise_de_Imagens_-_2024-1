ee5d7a13e3e93ee54d3b0d64261a9909460df52d
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-05-23 17:46:23 -0300

    Refatorando a classe FrameImagem para ter setters e getters e o método __imagem_atualizada.

--- app/visao/janela_principal.py
+++ app/visao/janela_principal.py
@@ -41,26 +41,62 @@ class FrameImagem(tkinter.Frame):
         :param controller: Controlador que instanciou esta classe.
         """
         tkinter.Frame.__init__(self, parent)
+        self.__image = None
+        self.__legenda = None
         self.on_resize = None
         self.controller = controller
         self.label = tkinter.Label(self)
         self.label.pack(fill="both", expand=True)
 
-    def set_imagem(self, image: PIL.Image.Image, legenda=None):
+    @property
+    def image(self):
         """
-        Atualiza a imagem do Frame Tkinter.
-        :param image: Nova imagem a ser adicionada.
-        :param legenda: Nova legenda a ser adicionada.
+        Obtém a imagem atual.
         :return:
         """
+        return self.__image
+
+    @image.setter
+    def image(self, image):
+        """
+        Altera a imagem atual.
+        :param image: Imagem a ser colocada no lugar.
+        :return:
+        """
+        self.__image = image
+        self.__imagem_atualizada()
+
+    @property
+    def legenda(self):
+        """
+        Obtém a legenda atual.
+        :return: Legenda atual.
+        """
+        return self.__legenda
+
+    @legenda.setter
+    def legenda(self, legenda):
+        """
+        Altera a legenda atual.
+        :param legenda: Legenda a ser colocada no lugar.
+        :return:
+        """
+        self.__legenda = legenda
+        self.__imagem_atualizada()
+
+    def __imagem_atualizada(self):
+        """
+        Ação executada quando a imagem é atualizada.
+        :return:
+        """
+        image = self.image
         label_width = self.label.winfo_width()
         label_height = self.label.winfo_height()
         if label_width > 1 and label_height > 1:
             image = image.resize((label_width, label_height))
         photo_image = PIL.ImageTk.PhotoImage(image)
-        self.label.config(image=photo_image, text=legenda)
+        self.label.config(image=photo_image, text=self.legenda)
         self.label.image = photo_image
-
 
 
 class JanelaPrincipal(tkinter.Tk):
@@ -168,5 +204,5 @@ class JanelaPrincipal(tkinter.Tk):
         :param image: Imagem a ser inserida.
         :type caminho: ImageTk.
         """
-        self.frames["FrameImagem"].set_imagem(image)
+        self.frames["FrameImagem"].image = image
         self.show_frame("FrameImagem")
