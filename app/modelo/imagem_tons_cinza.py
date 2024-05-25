facb3e2e32953893b1a6670bce140bb61dd81671
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-05-25 16:37:06 -0300

    Implementando método para gerar histograma de tons de cinza

--- app/modelo/imagem_tons_cinza.py
+++ app/modelo/imagem_tons_cinza.py
@@ -5,3 +5,17 @@ class ImagemTonsCinza(Imagem):
     """
     Representa uma matriz de tons de cinza.
     """
+
+    def to_histograma(self):
+        """
+        Gera um histograma da imagem.
+        """
+        histograma = {}
+        for linha in range(self.matriz.shape[0]):
+            for coluna in range(self.matriz.shape[1]):
+                pixel = self.matriz[linha][coluna]
+                if pixel in histograma:
+                    histograma[pixel] += 1
+                else:
+                    histograma[pixel] = 1
+        return histograma
