a01a3ea0f393a0f83f60ba93def85b0d8edaffb4
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-05-25 17:34:45 -0300

    Adicionando método para gerar histograma 2D

--- app/modelo/imagem_hsv.py
+++ app/modelo/imagem_hsv.py
@@ -37,3 +37,24 @@ class ImagemHSV(Imagem):
                     histograma_value[value] = 1
 
         return histograma_hue, histograma_saturation, histograma_value
+
+    def to_histograma_2d(self):
+        """
+        Gera um histograma 2D da imagem HSV.
+        """
+        histograma_2d = {}
+        for linha in range(self.matriz.shape[0]):
+            for coluna in range(self.matriz.shape[1]):
+
+                # Obtendo os valores de hue e value do pixel
+                hue, _, value = self.matriz[linha][coluna]
+                hue = (hue//16)*16
+                value = (value // 128) * 128
+
+                # Contando a quantidade de pixels com cada valor de hue e value
+                if (hue, value) in histograma_2d:
+                    histograma_2d[(hue, value)] += 1
+                else:
+                    histograma_2d[(hue, value)] = 1
+
+        return histograma_2d
