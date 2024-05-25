34ef950f02c3f56408e5696d80cecb39a3fce5dc
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-05-25 17:23:12 -0300

    Implementando intervalo personalizado no método to_histograma

--- app/modelo/imagem_tons_cinza.py
+++ app/modelo/imagem_tons_cinza.py
@@ -6,16 +6,19 @@ class ImagemTonsCinza(Imagem):
     Representa uma matriz de tons de cinza.
     """
 
-    def to_histograma(self):
+    def to_histograma(self, n_bin=16):
         """
         Gera um histograma da imagem.
+        :param n_bin: Número de caixas do histograma.
         """
         histograma = {}
+        intervalo = 256 // n_bin
         for linha in range(self.matriz.shape[0]):
             for coluna in range(self.matriz.shape[1]):
-                pixel = self.matriz[linha][coluna]
+                pixel = (self.matriz[linha][coluna] // intervalo) * intervalo
                 if pixel in histograma:
                     histograma[pixel] += 1
                 else:
                     histograma[pixel] = 1
         return histograma
+
