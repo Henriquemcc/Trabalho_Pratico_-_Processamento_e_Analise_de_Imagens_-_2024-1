ce3e2aebb893e7fd3d53f55668a5221e1f4d76d7
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-05-25 17:06:06 -0300

    Implementando método para gerar histograma de tons de HSV

--- app/modelo/imagem_hsv.py
+++ app/modelo/imagem_hsv.py
@@ -5,4 +5,35 @@ class ImagemHSV(Imagem):
     """
     Representa uma imagem HSV.
     """
-    pass
+    def to_histograma(self):
+        """
+        Gera um histograma da imagem HSV.
+        """
+        histograma_hue = {}
+        histograma_saturation = {}
+        histograma_value = {}
+        for linha in range(self.matriz.shape[0]):
+            for coluna in range(self.matriz.shape[1]):
+
+                # Obtendo os valores de hue, saturation e value do pixel
+                hue, saturation, value = self.matriz[linha][coluna]
+
+                # Contando a quantidade de pixels com cada valor de hue
+                if hue in histograma_hue:
+                    histograma_hue[hue] += 1
+                else:
+                    histograma_hue[hue] = 1
+
+                # Contando a quantidade de pixels com cada valor de saturation
+                if saturation in histograma_saturation:
+                    histograma_saturation[saturation] += 1
+                else:
+                    histograma_saturation[saturation] = 1
+
+                # Contando a quantidade de pixels com cada valor de value
+                if value in histograma_value:
+                    histograma_value[value] += 1
+                else:
+                    histograma_value[value] = 1
+
+        return histograma_hue, histograma_saturation, histograma_value
